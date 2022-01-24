b9d7a6fa89db6befcc72be29b3a1e6745ef03f99
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-24 21:42:29 +0900

    create topic部分のミニマムを作成

--- src/stip/api/__init__.py
+++ src/stip/api/__init__.py
@@ -9,20 +9,26 @@ from flask_cors import CORS, cross_origin
 
 # import original moduels
 from stip import app
+from stip.api.topic_management import TopicManagement
+from stip.api.topic import Topic
 
 
 # Class変数として利用するモジュールのインスタンスを作っておく
-# Topic = TopciCreate()
-
+# ここにでインスタンス化するものはモジュール内にself変数を持たない
+topic_management = TopicManagement()
 @app.route('/')
 def helloWorld():
   return "Hello World"
 
 @app.route('/publish/create', methods=["POST"])
 def topicCeate():
-  topic = request.get_json()
-  try:
-    if not (topic.keys() >= {'publisher', 'name', 'elements'}):
-      return 'Not Found Publisher or Name or Elements'
+  create_topic_request = request.get_json()
+  if not (create_topic_request.keys() >= {'publisher', 'name', 'elements'}):
+    return 'Not Found Publisher or Name or Elements'
 
-    # Topic Createメソッドの呼び出し
+  # Topic Createメソッドの呼び出し
+  topic = Topic()
+  topic.setTopicParameters(create_topic_request)
+  # Topicテーブルに対象のトピックレコードを追加
+  result = topic_management.create(topic)
+  return "topic: {0} created!!".format(topic.name)
