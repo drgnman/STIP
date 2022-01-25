a608bbbbfaa4e50fa664857d793b0c912d13d477
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-25 16:50:05 +0900

    DataValueTableへのデータ追加処理を追加

---
+++ src/stip/api/DataManagement.py
@@ -0,0 +1,23 @@
+from stip.utils.DBUtil import DBUtil
+import datetime
+import json
+
+class DataManagement:
+    def __init__(self):
+        pass
+
+    def insertToDataValue(self, data):
+        db = DBUtil()
+        db.createDBConnection()
+
+        # DataValueテーブルへのデータ追加
+        sql = "INSERT IGNORE INTO DATA_VALUE_TEMP (TOPIC_NAME, ELEMENT_VALUE) VALUES ('{0}', '{1}');".format(
+            data.topic_name, json.dumps(data.elements)
+        )
+        print(sql)
+        result = db.executeQuery(sql)
+        db.closeDBConnection()
+        return result
+    
+    def insertToSubscriberTopic(self, data):
+        pass
