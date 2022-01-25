7b808ba606a0b0647577219da1d6d767d40aaf93
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-25 19:19:47 +0900

    AMQPとMQTT(RabbitMQ利用)に対するpublish処理を追加

--- src/stip/api/PublishControl.py
+++ src/stip/api/PublishControl.py
@@ -1,6 +1,23 @@
+from stip.api.PublishToPlatforms import PublishToAMQP, PublishToMQTT
+
 class PublishControl:
     def __init__(self):
         pass
 
-    def publishDirectly(self, data):
-        pass
+    def publishDirectly(self, data, iot_type="amqp"):
+        if (iot_type == "amqp"):
+            self.publishToAQMP(data)
+        elif (iot_type == "mqtt"):
+            self.publishToMQTT(data)
+
+    def publishToAQMP(self, data):
+        publish_to_amqp = PublishToAMQP()
+        connection, channel = publish_to_amqp.createConnection()
+        publish_to_amqp.dataPublishToAMQP(channel, data)
+        return True
+
+    def publishToMQTT(self, data):
+        publish_to_mqtt = PublishToMQTT()
+        publish_to_mqtt.setConnection()
+        publish_to_mqtt.dataPublishToMQTT(data)
+        return True
