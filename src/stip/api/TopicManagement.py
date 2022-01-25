5bc7b49a8d5302a615b2cfe519979584672bf8bd
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-25 09:50:14 +0900

    creteメソッドの追加

--- src/stip/api/TopicManagement.py
+++ src/stip/api/TopicManagement.py
@@ -5,4 +5,14 @@ class TopicManagement:
     pass
 
   def create(self, topic):
-    pass
+    db = DBUtil()
+    db.createDBConnection()
+    sql = 'INSERT INTO TOPIC \
+            (TOPIC_NAME, TOPIC_TYPE, LATITUDE, LONGITUDE, EFFECTIVE_RANGE, DATA_TTL, PUBLISH_FREQUENCY, TOPIC_DESCRIPTION, PUBLISHER) \
+            VALUES ("{0}", "{1}", {2}, {3}, {4},{5}, {6}, "{7}", "{8}");'.format(
+              topic.name, topic.type, topic.latitude, topic.longitude, topic.effective_range,
+              topic.data_ttl, topic.publish_frequency, topic.description, topic.publisher
+            ) 
+    print(sql)
+    result  = db.executeQuery(sql)
+    return result
