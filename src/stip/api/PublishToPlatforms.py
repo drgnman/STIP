22fb75c9e62184bea89f57d5440ae1ed09510b48
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-25 19:20:27 +0900

    AMQPとMQTTのpublishファイルを統合，typoの修正

--- src/stip/api/PublishToPlatforms.py
+++ src/stip/api/PublishToPlatforms.py
@@ -1,4 +1,10 @@
+# AMQP
 import pika
+import json
+
+#MQTT
+import paho.mqtt.client as mqtt
+# from time import sleep # loop制御に使うかもしれない
 import json
 
 class PublishToAMQP:
@@ -6,7 +12,7 @@ class PublishToAMQP:
         pass
 
     def createConnection(self, ip='localhost', p='5672'):
-        connection = pika.BlockkingConnection(
+        connection = pika.BlockingConnection(
             # credentials = pika.PlainCredentials('username', 'password')
             pika.ConnectionParameters(host=ip, port=p)
         )
@@ -19,3 +25,15 @@ class PublishToAMQP:
     def dataPublishToAMQP(self, channel, data): # dataオブジェクトにはトピック名とelement_values(送信するデータ本体)が入っていること
         channel.exchange_declare(exchange=data.topic_name, exchange_type='fanout')
         channel.basic_publish(exchange=data.topic_name, routing_key='', body=json.dumps(data.element_values))
+
+class PublishToMQTT:
+    def __init__(self):
+        self.client = mqtt.Client()
+
+    def setConnection(self, ip="localhost", p=1883):
+        self.client.connect(ip, p, 60)
+
+    def dataPublishToMQTT(self, data):
+        self.client.loop_start()
+        self.client.publish(data.topic_name, json.dumps(data.element_values))
+        self.client.loop_stop()
