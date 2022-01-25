e5b30e8514431801bbbfd20e2876afbff31643fb
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-25 09:49:57 +0900

    effective_rangeの追加

--- src/stip/api/topic.py
+++ src/stip/api/topic.py
@@ -6,6 +6,7 @@ class Topic:       # Topicテーブルに関わる要素を初期化するため
     self.latitude = 0.0
     self.longitude = 0.0
     self.elements = ''
+    self.effective_range = 0.0
     self.data_ttl = 0            # 秒で指定
     self.publish_frequency = 0   # 秒で指定
     self.description = ''
@@ -19,6 +20,8 @@ class Topic:       # Topicテーブルに関わる要素を初期化するため
       self.longitude = create_topic_request['location']['longitude']
     if ('elements' in create_topic_request):          # 要素本体の確認
       self.elements = create_topic_request['elements']
+    if ('effectiveRange' in create_topic_request):           # effectiveRangeの確認
+      self.effective_range = create_topic_request['effectiveRange']
     if ('dataTTL' in create_topic_request):           # dataTTLの確認
       self.data_ttl = create_topic_request['dataTTL']
     if ('publishFrequency' in create_topic_request):  # publishFrequency(発信頻度)の確認
