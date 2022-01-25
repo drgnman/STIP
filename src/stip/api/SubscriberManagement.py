d23f8e6dc709ea9e2da94212b5c60e9b03763afa
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-25 20:20:41 +0900

    speedとdirectionも反映するように修正

--- src/stip/api/SubscriberManagement.py
+++ src/stip/api/SubscriberManagement.py
@@ -7,11 +7,13 @@ class SubscriberManagement:
     def registerSubscriber(self, subscriber):
         db = DBUtil()
         db.createDBConnection()
-        sql = 'INSERT IGNORE INTO SUBSCRIBERS (SUBSCRIBER, LATITUDE, LONGITUDE) VALUES \
-                ("{0}", {1}, {2});'.format(
+        sql = 'INSERT IGNORE INTO SUBSCRIBERS (SUBSCRIBER, LATITUDE, LONGITUDE, SPEED, DIRECTION) \
+            VALUES ("{0}", {1}, {2}, "{3}", "{4}");'.format(
                     subscriber.subscriber_name,
                     subscriber.latitude,
-                    subscriber.longitude                    
+                    subscriber.longitude,
+                    subscriber.speed,
+                    subscriber.direction 
                 )
         result = db.executeQuery(sql)
         return result
