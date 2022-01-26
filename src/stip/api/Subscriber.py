5099bf4faee01a38528428ae847d9c2135749296
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-26 14:30:39 +0900

    SubscriberTopic登録用の関数と変数を追加

--- src/stip/api/Subscriber.py
+++ src/stip/api/Subscriber.py
@@ -7,6 +7,13 @@ class Subscriber:
         self.speed = 0.0
         self.direction = 0.0
 
+        # "登録用の"SubscriberTopic要素
+        self.topic_list = {}
+        self.receive_frequency = ''
+        self.control_mode = ''     # Database嬢はPM_FLAG ("Contronlの手段を管理する")
+        self.detection_range = 0.0
+        self.data_ttr = 0       # 秒単位
+
     def setParameters(self, subscriber):
         self.subscriber_name = subscriber['SubscriberName']
         self.purpose = subscriber['Purpose']
@@ -17,3 +24,11 @@ class Subscriber:
             self.speed = subscriber['Speed']
         if ("Direction" in subscriber):
             self.direction = subscriber['Direction']
+        
+    def setSubscriberTopicParameters(self, subscriber):
+        # "登録用の"SubscriberTopic要素
+        self.topic_list = subscriber['TopicList']
+        self.receive_frequency = subscriber['ReceiveFrequency']
+        self.control_mode = subscriber['ControlMode']
+        self.detection_range = subscriber['DetectionRange']
+        self.data_ttr = subscriber['DataTTR']
