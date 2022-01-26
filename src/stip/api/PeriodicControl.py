0c3ca54bd9dd9602ce6b2054fdaf08992342e761
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-26 22:00:30 +0900

    if文条件式及び時間と分部分のintへのcastタイミングを変更

--- src/stip/api/PeriodicControl.py
+++ src/stip/api/PeriodicControl.py
@@ -11,18 +11,18 @@ class PeriodicControl:
         now_hour  = int(now.hour)
 
         receive_frequency = receive_frequency.split()
-        minute_part = int(receive_frequency[0])
-        hour_part = int(receive_frequency[1])
+        minute_part = receive_frequency[0]
+        hour_part = receive_frequency[1]
         # 時間の対象における一致を確認し，一致しなかった場合は処理を終了
-        if (not self.chechMatchHour(now_hour, hour_part)): return False
+        if (not self.checkMatchHour(now_hour, hour_part)): return False
 
         # 分単位の周期チェックを行い，一致したらTrue, 一致しない場合はFalseを返す
-        return self.checkMatchMinute(now_hour, now_minute, hour_part, minute_part, subscribe_start_time)
+        return self.checkMatchMinute(now_hour, now_minute, minute_part, subscribe_start_time)
     
 
     # 一旦，,と-の複雑な組み合わせについては検討とする
     def checkMatchHour(self, now_hour, hour_part):
-        if ("*" in hour_part): 
+        if ("*" == hour_part): 
             return True
 
         if ("," in hour_part):
@@ -43,10 +43,12 @@ class PeriodicControl:
     # (now_hour*60 + now_minute) 
     #   - (subscribe_start_time.hour*60 + subscribe_start_time.minute) % min_part == 0
     # 時間の期間に収まっているか，複数してしたどれと一致しているかは上で判定しているため，不要
-    def checkMatchMinute(self, now_hour, now_minute, min_part, subscribe_start_time):
+    def checkMatchMinute(self, now_hour, now_minute, minute_part, subscribe_start_time):
         # Subsciber_Topicを登録した時間
-        dt_subscribe_start_time = datetime.strptime(subscribe_start_time, '%Y-%m-%d %H:%M:%S')
-        if ( (now_hour*60 + now_minute) 
-            - (dt_subscribe_start_time.hour*60 + dt_subscribe_start_time.minute) % min_part == 0):
+        if ("*" == minute_part): 
+            return True
+
+        if (((now_hour*60 + now_minute) 
+            - (int(subscribe_start_time.hour)*60 + int(subscribe_start_time.minute))) % int(minute_part) == 0):
             return True
         return False
