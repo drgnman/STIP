d7145ff0174673893200c504febcdd8c54183000
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-26 21:26:12 +0900

    配列要素からオブジェクトに値をセットする関数の追加

--- src/stip/api/SubscriberTopic.py
+++ src/stip/api/SubscriberTopic.py
@@ -1,3 +1,5 @@
+import ast
+
 class SubscriberTopic:
     def __init__(self): 
         self.subscriber_topic_name = ''
@@ -11,6 +13,7 @@ class SubscriberTopic:
         self.data_ttr = 0
         self.detection_range = 0.0
         self.publish_timestamp = ''
+        self.create_timestamp = ''
 
     def setParameters(self, subscriber_topic):
         if ("SubscriberTopicName" in subscriber_topic):
@@ -20,11 +23,11 @@ class SubscriberTopic:
         if ("ExtractedTopicList" in subscriber_topic):
             self.extracted_topic_list = subscriber_topic['ExtractedTopicList']
         if ("ValueList" in subscriber_topic):
-            self.value_list = subscriber_topic['ValueList']
+            self.value_list = ast.literal_eval(subscriber_topic['ValueList'])
         if ("VariableList" in subscriber_topic):
-            self.variable_list = subscriber_topic['VariablaList']
+            self.variable_list = ast.literal_eval(subscriber_topic['VariablaList'])
         if ("ProcedureList" in subscriber_topic):
-            self.procedure_list = subscriber_topic['ProcedureList']
+            self.procedure_list = ast.literal_eval(subscriber_topic['ProcedureList'])
         if ("ControlMode" in subscriber_topic):
             self.control_mode = subscriber_topic['ControlMode']
         if ("ReceiveFrequency" in subscriber_topic):
@@ -33,3 +36,20 @@ class SubscriberTopic:
             self.data_ttr = subscriber_topic['DataTTR']
         if ("DetectionRange" in subscriber_topic):
             self.detection_range = subscriber_topic['DetectionRange']
+        if ("PublishTimestamp" in subscriber_topic):
+            self.publish_timestamp = subscriber_topic['PublishTimestamp']
+        if ("CreateTimestamp" in subscriber_topic):
+            self.create_timestamp = subscriber_topic['CreateTimestamp']
+
+    def setParameterFromList(self, subscriber_topic):
+        self.subscriber_topic_name = subscriber_topic[0]
+        self.topic_list = subscriber_topic[1]
+        self.extracted_topic_list = subscriber_topic[2]
+        self.value_list = ast.literal_eval(subscriber_topic[3])
+        self.procedure_list = ast.literal_eval(subscriber_topic[4])
+        self.control_mode = subscriber_topic[5]     # 旧 PM_Flag
+        self.receive_frequency = subscriber_topic[6]
+        self.data_ttr = subscriber_topic[7]
+        self.detection_range = subscriber_topic[8]
+        self.publish_timestamp = subscriber_topic[9]
+        self.create_timestamp = subscriber_topic[10]
