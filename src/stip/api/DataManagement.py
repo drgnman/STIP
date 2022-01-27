e4a55d29b144c7cc3e3cab533cc93621c492b308
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-27 18:43:14 +0900

    SubscriberTopicテーブルへのデータInsert処理を追加

--- src/stip/api/DataManagement.py
+++ src/stip/api/DataManagement.py
@@ -1,6 +1,11 @@
-from stip.utils.DBUtil import DBUtil
-import datetime
+from datetime import datetime
 import json
+import ast
+
+from pyrsistent import CheckedValueTypeError
+
+from stip.utils.DBUtil import DBUtil
+from stip.api.SubscriberTopic import SubscriberTopic
 
 class DataManagement:
     def __init__(self):
@@ -14,10 +19,56 @@ class DataManagement:
         sql = "INSERT IGNORE INTO DATA_VALUE_TEMP (TOPIC_NAME, ELEMENT_VALUE) VALUES ('{0}', '{1}');".format(
             data.topic_name, json.dumps(data.element_values)
         )
-        print(sql)
         result = db.executeQuery(sql)
         db.closeDBConnection()
         return result
     
     def insertToSubscriberTopic(self, data):
-        pass
+        db = DBUtil()
+        db.createDBConnection()
+
+        sql = 'SELECT SUBSCRIBER_TOPIC, PROCEDURE_LIST, VALUE_LIST FROM SUBSCRIBER_TOPICS;'
+        resultSet = db.fetchAllQuery(sql)
+        for result in resultSet:
+            subscriber_topic = SubscriberTopic()
+            subscriber_topic.subscriber_topic_name = result[0]
+            if (result[1] != None) : subscriber_topic.procedure_list = ast.literal_eval(result[1])
+            if (result[2] != None) : subscriber_topic.value_list = ast.literal_eval(result[2])
+
+            # print (subscriber_topic.procedure_list.values())
+            for element in subscriber_topic.procedure_list.values():
+                variable_list = element.get('VariableList')
+                print(variable_list)
+                if (variable_list == None): continue
+                print(variable_list)
+                for variable_element in variable_list:
+                    if (not self.checkTargetTopicInVariableList(data.topic_name, variable_list[variable_element]['TopicName'])): continue
+                    if (variable_element in subscriber_topic.value_list): 
+                    # value_listの該当キーに中身がない場合は新しくリストを用意する必要がある
+                        if (subscriber_topic.value_list[variable_element] != None):
+                            print("Elements", data.element_values.get(variable_list[variable_element]['Elements']))
+                            subscriber_topic.value_list[variable_element].insert(
+                                0, data.element_values.get(variable_list[variable_element]['Elements']))
+                            subscriber_topic.value_list[variable_element][-1] = str(datetime.now())
+                    # 一致しない場合，該当のvalue_listはNoneだけど，対象にはなっている
+                    else:
+                        subscriber_topic.value_list[variable_element] = [data.element_values.get(
+                            variable_list[variable_element]['Elements'])]
+                        print("ValueList: ", subscriber_topic.value_list[variable_element])
+                    
+            print(subscriber_topic.value_list)
+            if (subscriber_topic.value_list == {}): continue
+            sql = 'UPDATE SUBSCRIBER_TOPICS SET VALUE_LIST = \'{0}\' WHERE (SUBSCRIBER_TOPIC = "{1}")'.format(
+                json.dumps(subscriber_topic.value_list),
+                subscriber_topic.subscriber_topic_name
+            )
+            # subscriber_topic単位で最後にVALUE_LIST要素を更新する 
+            print(sql)
+            if (db.executeQuery(sql) == False): return False
+
+
+    def checkTargetTopicInVariableList(self, topic_name, variable_element):
+        if topic_name != variable_element:
+            return False
+        return True
+
