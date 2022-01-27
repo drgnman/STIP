a89ebe48a6e85e764749f4cf89eba136206ccdd3
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-27 09:56:10 +0900

    初回コミット, 具体的な処理はまだ

---
+++ src/stip/utils/MathOperator.py
@@ -0,0 +1,32 @@
+import numpy as np
+import statistics
+import math
+
+class MathOperator:
+    def __init__(self):
+        pass
+    
+def calculateCall(self, a, b):
+    pass
+    # if(op == "Hot"):
+    #     return self.Hot()
+    # elif(op == "Add"):
+    #     return self.Add()
+    # elif(op == "Min"):
+    #     return self.Minus()
+    # elif(op == "Mul"):
+    #     return self.MultiPlication()
+    # elif(op == "Div"):
+    #     return self.Division()
+    # elif(op == "SUM"):
+    #     return self.Sum()
+    # elif(op == "AVE"):
+    #     return self.Average()
+    # elif(op == "MAX"):
+    #     return self.Maximum()
+    # elif(op == "MIN"):
+    #     return self.Minimum()
+    # elif(op == "MID"):
+    #     return self.Midian()
+    # elif(op == "MOD"):
+    #     return self.Mode()
