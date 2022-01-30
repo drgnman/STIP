6b23b60052d538922b92bc3b32ea2c39fa52e213
drgnman <drgnman@sfc.keio.ac.jp>
2022-01-30 11:22:18 +0900

    集計処理を持ったパブリッシュまでの一連の動作を作成

--- src/stip/api/PeriodicPublishBySubscriberTopic.py
+++ src/stip/api/PeriodicPublishBySubscriberTopic.py
@@ -1,4 +1,5 @@
-from os import openpty
+from datetime import datetime
+from stip.api.Data import Data
 from stip.api import ProcedureProcessing
 from stip.utils.DBUtil import DBUtil
 from stip.api.PeriodicControl import PeriodicControl
@@ -7,9 +8,10 @@ from stip.api.SubscriberTopic import SubscriberTopic
 from stip.api.ProcedureProcessing import ProcedureProcessing
 
 class PeriodicPublishBySubscriberTopic:
-    periodic_control = PeriodicControl()
     def __init__(self):
         self.procedureProcessing = ProcedureProcessing()
+        self.periodic_control = PeriodicControl()
+        self.publish_control = PublishControl()
 
     def PublishBySubscriberTopic(self):
         db = DBUtil()
@@ -22,17 +24,18 @@ class PeriodicPublishBySubscriberTopic:
             subscriber_topic.setParameterFromList(record)
             if not (self.periodic_control.judgeToPublishTarget(subscriber_topic.receive_frequency, subscriber_topic.create_timestamp)):
                 continue
-            # 集計処理及び
             # パブリッシュの処理を書いていく
+            # ここに最後パブリッシュ内容をまとめて，最後にパブリッシュ
+            publish_contents = {}
+
+            # Procedureに記載された処理の実行
             for topic_name in subscriber_topic.procedure_list:
                 procedure = subscriber_topic.procedure_list[topic_name]['Procedure']
                 if ("VariableList" in subscriber_topic.procedure_list[topic_name]):
                     variable_list = subscriber_topic.procedure_list[topic_name]['VariableList']
                 operators = self.procedureProcessing.procedureSplit(procedure)
                 for operator in operators:
-                    print("operator: ", operator)
                     # operatorを参考に計算する箇所の切り出し
-                    print("procedure:", procedure)
                     target_formula = self.procedureProcessing.extractTargetFormulaPart(
                         operator, procedure
                         )
@@ -43,16 +46,24 @@ class PeriodicPublishBySubscriberTopic:
                     else:
                         rewrite_elements = target_formula
 
-                    print("rewrite_elements: ", rewrite_elements)
                     # 計算処理
                     processingResult = self.procedureProcessing.calcurateFromProcedure(
                         operator, rewrite_elements
                         )
-                    print("processing result: ", processingResult)
                     # 結果を用いてprocedure本体を書き換える
                     procedure = self.rewriteProcedureByCulculatedResult(
                         operator, procedure, target_formula, processingResult 
                         )
+                publish_contents[topic_name] = [procedure, str(datetime.now())]
+                
+            # 完成したデータとトピック名をDataオブジェクトに格納してIoTプラットフォームにPublish
+            publish_data = Data()
+            publish_data.topic_name = subscriber_topic.subscriber_topic_name
+            publish_data.element_values = publish_contents
+            print(publish_data.element_values)
+
+            result = self.publish_control.publishDirectly(publish_data)
+            print(result)
 
     def rewriteProcedureByCulculatedResult(self, operator, procedure, target_formula, result):
         replace_target = "{0}({1})".format(operator, target_formula)
