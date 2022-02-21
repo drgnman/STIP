e908a8adcfe51e2c8ab3a710ee24e3a22b1d6218
drgnman <drgnman@sfc.keio.ac.jp>
2022-02-21 16:15:22 +0900

    状況に応じた位置情報書き換えメソッド4つを追加

--- src/stip/utils/ProccessingSupports.py
+++ src/stip/utils/ProccessingSupports.py
@@ -9,3 +9,24 @@ class ProcessingSupports:
         remove_space = remove_double_quote.replace(" ", "")
         convert_to_list = remove_space.split(',')
         return convert_to_list
+
+    def nextLatitudeLargerThanCurrentLatitude(self, base_latitude, latitude):
+        if (latitude < base_latitude):
+            return latitude
+        return base_latitude
+
+    def nextLatitudeSmallerThanCurrentLatitude(self, base_latitude, latitude):
+        if (base_latitude < latitude):
+            return latitude
+        return base_latitude
+
+    def nextLongitudeLagerThanCurrentLongitude(self, base_longitude, longitude):
+        if (longitude < base_longitude):
+            return longitude
+        return base_longitude
+
+    def nextLongitudeSmallerThanCurrentLongitude(self, base_longitude, longitude):
+        if (base_longitude < longitude):
+            return longitude
+        return base_longitude
+    
